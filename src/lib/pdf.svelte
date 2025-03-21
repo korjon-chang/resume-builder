421c88eb723889acde7da81f70252b6cd658828c
Code of K <korjon0000@gmail.com>
2025-03-20 20:21:35 -0400

    dimension of template now fits the window of small screen

--- src/lib/pdf.svelte
+++ src/lib/pdf.svelte
@@ -2,9 +2,9 @@
 	import { formState } from '$lib/state.svelte';
 </script>
 
-<div class="flex h-full w-full items-center justify-center overflow-auto">
-	<div class="bg-white p-2">
-		<h1 class="text-3xl font-bold">formState</h1>
+<div class="flex h-full w-full overflow-auto">
+	<div class="m-auto h-[1123px] w-[793px] bg-white p-2">
+		<h1 class="text-3xl font-bold">{formState.personalInfo.firstName}</h1>
 		<p class="text-lg">Web Developer | john.doe@example.com</p>
 
 		<h2 class="mt-6 text-xl font-semibold">Experience</h2>
